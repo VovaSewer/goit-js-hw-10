344c0c34fa742cc15b697d7efc28c9afd119330e
VovaSewer <shutko7@gmail.com>
2022-02-15 11:19:24 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import countriesCard from './country_card';
+import countryCard from './counrty.hbs';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';    
 
 const DEBOUNCE_DELAY = 300;
